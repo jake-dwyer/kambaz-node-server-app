a26a9f76973066e928b711f4bba1b59b91db5a80
jake <jakedwyer7@gmail.com>
2025-04-14 17:38:12 -0400

    im dumb

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import session from "express-session";
 import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
-
-import Hello from "./Hello.js";
+import Hello from "./Hello.js"
 import Lab5 from "./Labs/Lab5/index.js";
 import UserRoutes from './Kambaz/Users/routes.js';
 import CourseRoutes from './Kambaz/Courses/routes.js';
@@ -12,29 +11,16 @@ import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
-
-const app = express();
-
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://a6--cosmic-donut-a20d76.netlify.app',
-];
-
+const app = express()
 app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error(`CORS error: origin ${origin} not allowed`));
-      }
-    },
-  })
+    cors({
+        credentials: true,
+        origin:  process.env.NETLIFY_URL,
+    })
 );
-
 const isDev = process.env.NODE_ENV === "development";
 
 const sessionOptions = {
@@ -43,7 +29,7 @@ const sessionOptions = {
   saveUninitialized: false,
   cookie: {
     sameSite: isDev ? "lax" : "none",
-    secure: !isDev,
+    secure: !isDev,                    
   },
 };
 
@@ -52,16 +38,22 @@ if (!isDev) {
   sessionOptions.cookie.domain = process.env.NODE_SERVER_DOMAIN;
 }
 
-app.use(session(sessionOptions));
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
-
-Hello(app);
-Lab5(app);
+const port = process.env.PORT || 4000;
+Hello(app)
+Lab5(app)
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
-
-const port = process.env.PORT || 4000;
-app.listen(port);
+app.listen(port)
