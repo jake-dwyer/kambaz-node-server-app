d3465c9335aa42c08b2669b8d49a08bf1ebaf42f
jake <jakedwyer7@gmail.com>
2025-04-14 18:58:22 -0400

    help

--- index.js
+++ index.js
@@ -56,4 +56,4 @@ CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
-app.listen(port)
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!`));
