ab5aa332d946cf7781c6dd7455b124cc6f2f755c
jake <jakedwyer7@gmail.com>
2025-04-14 19:46:18 -0400

    .

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -3,33 +3,27 @@ import * as modulesDao from "../Modules/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
-  // Create a course and enroll the current user
   app.post("/api/courses", async (req, res) => {
-    // Create the course using data from the request body.
     const course = await dao.createCourse(req.body);
     const currentUser = req.session["currentUser"];
     console.log("Creating course. currentUser:", currentUser, "course:", course);
-    // If a user is logged in, enroll them in the newly created course.
     if (currentUser) {
       await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
     }
     res.json(course);
   });
 
-  // Retrieve all courses
   app.get("/api/courses", async (req, res) => {
     const courses = await dao.findAllCourses();
     res.json(courses);
   });
 
-  // Delete a course by its ID
   app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const status = await dao.deleteCourse(courseId);
     res.send(status);
   });
 
-  // Update a course by its ID
   app.put("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const courseUpdates = req.body;
@@ -37,14 +31,12 @@ export default function CourseRoutes(app) {
     res.send(status);
   });
 
-  // Get modules for a given course by courseId
   app.get("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
     const modules = await modulesDao.findModulesForCourse(courseId);
     res.json(modules);
   });
     
-  // Create a new module for a course
   app.post("/api/courses/:courseId/modules", async (req, res) => {
     const { courseId } = req.params;
     const module = {
@@ -55,7 +47,6 @@ export default function CourseRoutes(app) {
     res.send(newModule);
   });
 
-  // Get all users enrolled in a given course, using 'cid' as the parameter name.
   app.get("/api/courses/:cid/users", async (req, res) => {
     const { cid } = req.params;
     const users = await enrollmentsDao.findUsersForCourse(cid);
