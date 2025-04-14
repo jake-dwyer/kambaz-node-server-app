6531c8d17b7c1842e5bdba321a2dc8a9bee87385
jake <jakedwyer7@gmail.com>
2025-04-14 18:13:51 -0400

    reset

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import session from "express-session";
 import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config";
-import Hello from "./Hello.js";
+import Hello from "./Hello.js"
 import Lab5 from "./Labs/Lab5/index.js";
 import UserRoutes from './Kambaz/Users/routes.js';
 import CourseRoutes from './Kambaz/Courses/routes.js';
@@ -11,34 +11,17 @@ import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
-
-const app = express();
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://a6--cosmic-donut-a20d76.netlify.app"
-];
-
+const app = express()
 app.use(
-  cors({
-    credentials: true,
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    }
-  })
+    cors({
+        credentials: true,
+        origin:  process.env.NETLIFY_URL || "http://localhost:5173",
+    })
 );
-
 const isDev = process.env.NODE_ENV === "development";
-
-if (!isDev) {
-  app.set("trust proxy", 1); // ⬅️ this is CRUCIAL for trusting session cookies via Render
-}
 
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
@@ -46,25 +29,31 @@ const sessionOptions = {
   saveUninitialized: false,
   cookie: {
     sameSite: isDev ? "lax" : "none",
-    secure: !isDev,
-  }
+    secure: !isDev,                    
+  },
 };
 
 if (!isDev) {
+  sessionOptions.proxy = true;
   sessionOptions.cookie.domain = process.env.NODE_SERVER_DOMAIN;
 }
 
-app.use(session(sessionOptions));
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+app.use(session(sessionOptions));  
 app.use(express.json());
-
 const port = process.env.PORT || 4000;
-
-Hello(app);
-Lab5(app);
+Hello(app)
+Lab5(app)
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
-
-app.listen(port);
+app.listen(port)
