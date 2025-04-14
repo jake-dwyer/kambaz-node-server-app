d2c12a0692dcc0e890af2a80af5bfd91a61d28d1
jake <jakedwyer7@gmail.com>
2025-04-14 19:08:49 -0400

    help

--- index.js
+++ index.js
@@ -11,49 +11,47 @@ import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
 
-
-const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
-const app = express()
-app.use(
-    cors({
-        credentials: true,
-        origin:  process.env.NETLIFY_URL
-    })
-);
+
+const app = express();
 const isDev = process.env.NODE_ENV === "development";
 
+app.use(
+  cors({
+    credentials: true,
+    origin: isDev ? "http://localhost:5173" : process.env.NETLIFY_URL,
+  })
+);
+
 const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
   cookie: {
     sameSite: isDev ? "lax" : "none",
-    secure: !isDev,                    
+    secure: !isDev,
   },
 };
 
 if (!isDev) {
+  app.set("trust proxy", 1);
   sessionOptions.proxy = true;
   sessionOptions.cookie.domain = process.env.NODE_SERVER_DOMAIN;
 }
 
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
-    };
-  }
-app.use(session(sessionOptions));  
+app.use(session(sessionOptions));
 app.use(express.json());
-const port = process.env.PORT || 4000;
-Hello(app)
-Lab5(app)
+
+Hello(app);
+Lab5(app);
 UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
-app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}!`));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}!`);
+});
