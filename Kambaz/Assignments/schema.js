a133fd0b5997ce25e7e088e299c2a0ea2c1913c0
jake <jakedwyer7@gmail.com>
2025-04-15 17:17:28 -0400

    yur

--- Kambaz/Assignments/schema.js
+++ Kambaz/Assignments/schema.js
@@ -1,9 +1,26 @@
 import mongoose from "mongoose";
+
 const assignmentSchema = new mongoose.Schema({
-   _id: String,
-   title: String,
-   course: String,
- },
- { collection: "assignments" }
-);
+  _id: String,
+  title: String,
+  course: String,
+  module: String,
+  points: Number,
+  group: String,
+  displayGrade: String,
+  submissionType: String,
+  options: {
+    textEntry: Boolean,
+    websiteURL: Boolean,
+    mediaRecordings: Boolean,
+    studentAnnotation: Boolean,
+    fileUploads: Boolean,
+  },
+  assignTo: String,
+  dueDate: String,
+  availableFrom: String,
+  availableUntil: String,
+  description: String,
+}, { collection: "assignments" });
+
 export default assignmentSchema;
